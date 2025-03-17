95a330021d18c07f7320e19fb5d5eec6e2c1fd39
Andrew Chmutov <ulman.andre@gmail.com>
2025-03-17 15:21:17 +0100

    perf: lazy load

--- big_medicine/processing.py
+++ big_medicine/processing.py
@@ -1,5 +1,9 @@
-import numpy as np
-import pandas as pd
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    import pandas as pd
 
 
 def prepare(
@@ -7,5 +11,7 @@ def prepare(
     low: int,
     high: int | None = None,
 ) -> pd.DataFrame:
+    import numpy as np
+
     data["count"] = np.random.randint(low, high, size=data.shape[0])
     return data
